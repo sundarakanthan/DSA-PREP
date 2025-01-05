1d6b5ae1379728c7182ea513bcd8b42b04660555
sundarakanthan <sundarakanthan88@gmail.com>
2025-01-05 21:58:49 +0530

    math files added

---
+++ Math-for-dsa/dsaMath.java
@@ -0,0 +1,13 @@
+public class dsaMath{
+public static void main(String[] args) {
+   System.out.println(isPrime(274));
+}
+public static  boolean isPrime(int n){
+   for(int i=2;i<n/2;i++){
+    if(n%i==0){
+        return false;
+    }
+   }
+    return true;       
+}
+}
